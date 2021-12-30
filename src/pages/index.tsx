5bd3be3aca44cb966dbe3508bf7ed66ca38f35bc
yang.ouyang <yang.ouyang@seamoney.com>
2021-12-30 15:26:26 +0800

    mian

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import styles from './index.less';
+import React from 'react';
+import { Button } from 'antd';
+import './App.css';
+
+const App = () => (
+  <div className="App">
+    <Button type="primary">Button</Button>
+  </div>
+);
+
+export default App;
